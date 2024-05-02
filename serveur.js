11149c0800776490e2ca72410dc43f4d1ffd36f2
Aurlien-V <153271619+Aurlien-V@users.noreply.github.com>
2024-05-02 13:30:09 +0200

    1

--- serveur.js
+++ serveur.js
@@ -11,12 +11,45 @@ const swaggerJsdoc = require('swagger-jsdoc');
 const options = require('./swagger.json');
 const specs = swaggerJsdoc(options);
 
-const sequelize = new Sequelize('database', 'username', 'password', {
+const Music = require('./models/music.js');
+
+(async () => {
+  //const music1 = await Music.create({ cover: "harry_styles-watermelon_sugar.jpg", sound: "Harry_Styles-Watermelon_Sugar.mp3", title: "Harry Styles - Watermelon Sugar", category: "pop"});
+
+  //console.log(await Music.findAll());
+})();
+
+
+/*-----FUNCTION----*/
+function myCors(req, res, nxt) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,POST,PUT,OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Access-Control-Allow-Origin, Content-Type, Accept, Accept-Language, Origin, User-Agent');
+  if(req.method === 'OPTIONS') {
+      res.sendStatus(204);
+  }
+  else {
+      nxt();
+  }
+}
+
+(async function dbConnect() {
+  const sequelize = new Sequelize('database', 'username', 'password', {
     host: 'localhost',
     dialect: 'sqlite',
+    storage: './db/database.sqlite'
   });
+  
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
 
-app.use(cors())
+/*-----CODE----*/
+app.use(myCors);
 app.use(express.json());
 app.use(express.urlencoded({extended: true}));
 app.use(`/api/${version}`, router);
