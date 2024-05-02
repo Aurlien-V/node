6e8b6e886c913ede8a71cbc451b601c4d15850ce
Aurlien-V <153271619+Aurlien-V@users.noreply.github.com>
2024-05-02 09:33:09 +0200

    1

--- serveur.js
+++ serveur.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const app = express()
 const port = 3000
+const port = process.env.PORT  || 4000;
 const version = "v1"
 const router = require('./routes/routes');
 const swaggerUi = require('swagger-ui-express');
